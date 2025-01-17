97a97b16cf33caa8593e3959ec36cc931f8ff56d
Michae Dörr <michael@midoerr.de>
2025-01-18 00:48:20 +0100

    add first plrat pass

---
+++ src/trusted/main_plrat_first.c
@@ -0,0 +1,31 @@
+
+#include <stdbool.h>          // for bool, false
+#include <stdio.h>            // for fflush, stdout
+//#include "trusted_checker.h"  // for tc_init, tc_run
+#include "trusted_utils.h"    // for trusted_utils_try_match_arg, trusted_ut...
+//#if IMPCHECK_WRITE_DIRECTIVES
+//#include <unistd.h>
+//#include "../writer.h"
+//#endif
+
+int main(int argc, char *argv[]) {
+    // path/to/formula.cnf path/to/proofs/ <num-solvers> <solver-id> <redistribution-strategy>
+    const char *formula_path = "", *proofs_path = "";
+    u64 num_solvers = 0, solver_id = 0, redistribution_strategy = 0;
+    for (int i = 1; i < argc; i++) {
+        trusted_utils_try_match_arg(argv[i], "-formula-path=", &formula_path);
+        trusted_utils_try_match_arg(argv[i], "-proofs-path=", &proofs_path);
+        trusted_utils_try_match_num(argv[i], "-num-solvers=", &num_solvers);
+        trusted_utils_try_match_num(argv[i], "-solver-id=", &solver_id);
+        trusted_utils_try_match_num(argv[i], "-redistribution-strategy=", &redistribution_strategy);
+    }
+
+
+    char output_path[512];
+    snprintf(output_path, 512, "-formula-path=%s -proofs-path=%s num-solvers=%lu -solver-id=%lu -redistribution-strategy=%lu",
+    formula_path, proofs_path, num_solvers, solver_id, redistribution_strategy);
+    trusted_utils_log(output_path);
+
+    fflush(stdout);
+    return 0;
+}
